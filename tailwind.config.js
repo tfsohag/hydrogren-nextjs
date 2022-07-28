e93ce613eb6c23653efa7fb00d699ab887732f99
tfsomrat <themefisher.dev@gmail.com>
2022-07-28 12:30:54 +0600

    update font settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,8 +47,11 @@ module.exports = {
       fontSize: {
         base: font_base + "px",
         h1: h1 + "rem",
+        "h1-sm": h1 * 0.8 + "rem",
         h2: h2 + "rem",
+        "h2-sm": h2 * 0.8 + "rem",
         h3: h3 + "rem",
+        "h3-sm": h3 * 0.8 + "rem",
         h4: h4 + "rem",
         h5: h5 + "rem",
         h6: h6 + "rem",
