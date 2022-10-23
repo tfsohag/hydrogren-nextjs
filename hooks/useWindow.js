0b21fe00805cc9844b75828e31af58685880fee2
tfsomrat <themefisher.dev@gmail.com>
2022-10-23 15:50:14 +0600

    update hooks

---
+++ hooks/useWindow.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+// useWindow(768)
+const useWindow = (size) => {
+  // getWindowDimensions
+  const [windowSize, setWindowSize] = useState(size || 768);
+  useEffect(() => {
+    function viewport() {
+      var width = Math.max(
+        document.documentElement.clientWidth,
+        window.innerWidth || 0
+      );
+      setWindowSize(width);
+    }
+    viewport();
+    window.onresize = viewport;
+  }, []);
+
+  return windowSize;
+};
+
+export default useWindow;
