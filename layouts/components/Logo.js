cdbb386f4ce1e88df6ca031e7ffd3726a25bed79
tfsomrat <themefisher.dev@gmail.com>
2022-11-27 09:59:36 +0600

    modified logo

--- layouts/components/Logo.js
+++ layouts/components/Logo.js
@@ -1,28 +1,24 @@
+import ImageFallback from "@components/ImageFallback";
 import config from "@config/config.json";
-import Image from "next/image";
 import Link from "next/link";
 
 const Logo = ({ src }) => {
   // destructuring items from config object
-  const { base_url, logo, logo_width, logo_height, logo_text, title } =
-    config.site;
+  const { logo, logo_width, logo_height, logo_text, title } = config.site;
 
   return (
-    <Link
-      href={base_url}
-      className="navbar-brand block"
-      style={{
-        height: logo_height.replace("px", "") + "px",
-        width: logo_width.replace("px", "") + "px",
-      }}
-    >
+    <Link href="/" className="navbar-brand block">
       {src || logo ? (
-        <Image
+        <ImageFallback
           width={logo_width.replace("px", "") * 2}
           height={logo_height.replace("px", "") * 2}
           src={src ? src : logo}
           alt={title}
           priority
+          style={{
+            height: logo_height.replace("px", "") + "px",
+            width: logo_width.replace("px", "") + "px",
+          }}
         />
       ) : logo_text ? (
         logo_text
